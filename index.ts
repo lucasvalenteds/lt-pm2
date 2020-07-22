912f65fe6f7ed32f39b1a20e2a7b3e26841acb84
Lucas <lucasvalenteds@gmail.com>
2020-07-22 09:09:35 -0300

    Routing request to different paths
    * Requests to / returns Hello World in JSON
    * Requests to /shutdown stops the server
    * Requests to other paths returns not found

--- index.ts
+++ index.ts
@@ -1,11 +1,44 @@
 import * as HTTP from "http";
+import * as URL from "url";
 
 const port = process.env.PORT;
 
-const server = HTTP.createServer((_, response) => {
+const index: HTTP.RequestListener = (_request, response): void => {
   response.statusCode = 200;
+  response.setHeader("Content-Type", "application/json");
   response.write(JSON.stringify({ message: "Hello World" }));
-  response.end(() => console.debug("%s: Request received", new Date()));
+  response.end(() => {
+    console.debug("%s: Request received to index", new Date());
+  });
+};
+
+const shutdown: HTTP.RequestListener = (_request, response): void => {
+  response.statusCode = 204;
+  response.end(() => {
+    console.debug("%s: Request received to shutdown", new Date());
+    process.exit(0);
+  });
+};
+
+const notFound: HTTP.RequestListener = (_request, response): void => {
+  response.statusCode = 404;
+  response.end(() => {
+    console.debug("%s: Request ended with status 404", new Date());
+  });
+};
+
+const server = HTTP.createServer((request, response): void => {
+  const url = URL.parse(request.url!);
+
+  if (url.path === "/") {
+    return index(request, response);
+  }
+
+  if (url.path === "/shutdown") {
+    return shutdown(request, response);
+  }
+
+  return notFound(request, response);
 });
 
 server
